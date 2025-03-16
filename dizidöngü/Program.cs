9dc9be81f072c6e80629a61c8b753aa6eb3e6c1e
iavci <iavci@LAPTOP-4674SJUE>
2025-03-16 13:58:40 +0300

    Add project files.

---
+++ dizidöngü/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 5 sayı için dizi oluşturuluyor
+        int[] sayilar = new int[5];
+        int toplam = 0;
+
+        // Kullanıcıdan 5 sayı girmesi isteniyor
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Lütfen {i + 1}. sayıyı girin: ");
+            sayilar[i] = int.Parse(Console.ReadLine());  // Girilen sayıyı diziye ekliyoruz
+        }
+
+        // Döngü ile tüm sayılar toplanıyor
+        foreach (int sayi in sayilar)
+        {
+            toplam += sayi;
+        }
+
+        // Ortalama hesaplanıyor
+        double ortalama = toplam / 5.0;
+
+        // Sonuç ekrana yazdırılıyor
+        Console.WriteLine($"Girdiğiniz sayıların ortalaması: {ortalama}");
+    }
+}
